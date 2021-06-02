632f56ea69a2c2bd0e9b3502acc1aa2a7118a698
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2021-06-02 14:31:49 -0300

    cars model

---
+++ src/main/java/com/example/task/model/Car.java
@@ -0,0 +1,49 @@
+package com.example.task.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="cars")
+public class Car {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String name;
+    @Column
+    private Integer year;
+    @Column
+    private String color;
+
+    public Car() {}
+
+    public Car(String name, Integer year, String color) {
+        this.name = name;
+        this.year = year;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
