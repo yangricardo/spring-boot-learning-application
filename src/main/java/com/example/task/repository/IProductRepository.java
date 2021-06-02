ddb0468b4c7b97382a77e474abd8555f8dedf4f9
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2021-06-02 15:57:22 -0300

    product repository

---
+++ src/main/java/com/example/task/repository/IProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.task.repository;
+
+import com.example.task.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductRepository extends JpaRepository<Product, Long> {
+}
